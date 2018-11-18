3fc238402d90044cdbd69016f4238bcf769777a0
Arthur Saveliev <arthur.saveliev@gmail.com>
2018-11-18 03:31:07 +0100

    adds training to model creator

--- engine/training.py
+++ engine/training.py
@@ -5,7 +5,9 @@ import cv2
 import numpy as np
 
 from keras import utils
-from utils import utils_image
+from utils import utils_image, utils_train
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from keras.models import Sequential
 
 
 def list_images(path, extensions=['.png', '.jpg', '.jpeg']):
@@ -34,118 +36,14 @@ def list_folders_with_images(path):
     return folders
 
 
-class ModelCreator:
-    path = None
-    model_name = None
-    model_prefix = "model_"
-    exts = ['.png', '.jpg', '.jpeg']
-    save_extension = ".png"
-
-    def __init__(self, path, model_name=None):
-        self.path = os.path.abspath(path)
-        self.set_model_name_or_default(model_name)
-
-    def save_component(self, name, images, replace=True, verbose=False):
-
-        """
-        Save component images in a folder named after the component.
-
-        :param name: component/folder name
-        :param images: component images
-        :param replace: if true, it will replace images from an existing folder
-        :param verbose: prints debug details
-        :return:
-        """
-
-        if not name:
-            print("Provided component name is not valid")
-            return
-        if images is None:
-            print("No images provided")
-            return
-
-        removed = 0
-        added = 0
-
-        path = self.__create_folder_if_needed(self.get_model_path(), name)
-        current_images, current_images_size = list_images(path)
-
-        # remove previous
-        if replace and current_images_size > 0:
-            for image in current_images:
-                os.remove(image)
-                removed += 1
-            current_images, current_images_size = list_images(path)
-
-        for idx, image in enumerate(images):
-            image_name = "{}_{}{}".format(name, idx + current_images_size, self.save_extension)
-            image_path = os.path.join(path, image_name)
-
-            cv2.imwrite(image_path, image)
-            added += 1
-
-        current_images, current_images_size = list_images(path)
-
+def create_folder_if_needed(path, name, verbose=False):
+    folder = os.path.join(path, name)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
         if verbose:
-            print("deleted: {}, added: {}, total: {} ({})".format(removed, added, current_images_size, path))
+            print("Created dir: {}".format(folder))
 
-    def remove_component(self, name):
-        """
-        Deletes component folder and it's content
-        :param name: component name
-        """
-
-        path = self.get_model_path()
-        component_path = os.path.join(path, name)
-        if os.path.exists(component_path):
-            shutil.rmtree(component_path)
-            return True
-        return False
-
-    def set_model_name_or_default(self, name=None):
-        """
-        Creates component folder. If no name is provided and one is generated
-        :param name: component folder name
-        :return:
-        """
-        if not name:
-            folders, prefixed = self.__list_model_folders()
-            self.model_name = "{}{}".format(self.model_prefix, len(prefixed))
-        else:
-            self.model_name = name
-
-        self.__create_folder_if_needed(self.path, self.model_name)
-
-    @staticmethod
-    def __create_folder_if_needed(path, name, verbose=False):
-        folder = os.path.join(path, name)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-            if verbose:
-                print("Create dir: {}".format(folder))
-
-        return folder
-
-    def __list_model_folders(self):
-        """
-        :return: a list of folders located in project root
-        """
-        folders = []
-        prefixed_folders = []
-        root = self.path
-        for item in os.listdir(root):
-            if os.path.isdir(os.path.join(root, item)) and "." not in item:
-                if item.startswith(self.model_prefix):
-                    prefixed_folders.append(item)
-                folders.append(os.path.join(root, item))
-
-        return folders, prefixed_folders
-
-    def get_model_path(self):
-        return os.path.join(self.path, self.model_name)
-
-    def list_model_components(self):
-        return list_folders_with_images(self.get_model_path())
+    return folder
 
 
 class DataLoader:
@@ -305,3 +203,215 @@ class DataLoader:
         test_labels = labels[0: vfold_size]
 
         return train, train_labels, test, test_labels
+
+
+class ModelTrain:
+
+    model = None
+    model_name = "model.h5"
+
+    def train(self, x_train, y_train, verbose=2, epochs=5, validation_split=0.1, batch_size=256):
+
+        shape = x_train.shape[1:]
+        outcomes = y_train.shape[1]
+
+        model = self.__build_model(shape=shape, outcomes=outcomes)
+        model.fit(x=x_train, y=y_train, validation_split=validation_split, batch_size=batch_size,
+                  verbose=verbose, epochs=epochs)
+
+        self.model = model
+
+        return model
+
+    def evaluate(self, x_test, y_test, model=None):
+
+        if model is None and self.model is not None:
+            model = self.model
+        elif model is None:
+            print("Model not found")
+            return
+
+        predictions = model.predict(x_test)
+        accuracy = utils_train.calculate_accuracy(predictions, y_test)
+
+        return accuracy
+
+    def save(self, name=None, path=None, model=None):
+
+        if model is None and self.model is not None:
+            model = self.model
+        elif model is None:
+            print("Missing model, nothing to save")
+            return
+
+        if name is None and self.model_name is not None:
+            name = self.model_name
+        elif self.model_name is None:
+            print("Please provide a valid model name to save")
+            return
+
+        # add extension
+        if not name.endswith(".h5"):
+            name += ".h5"
+
+        if not path:
+            path = name
+        else:
+            path = os.path.join(path, name)
+
+        print("Saved model: {}".format(path))
+        model.save(path)
+
+    def get_model(self):
+        return self.model
+
+    @staticmethod
+    def __build_model(shape, outcomes, verbose=False):
+
+        """
+        Builds base model for greyscale (single channel) images
+
+        shape: image shape (h, w, channel) i.e (28, 28, 1)
+        outcomes: possible numeric outcomes/labels i.e [0, 1, 2, 3, 5, 6] (normalized)
+
+        :return: compiled model
+        """
+
+        # define model
+        model = Sequential()
+        model.add(Conv2D(16, kernel_size=(3, 3), padding='same', input_shape=shape, activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(outcomes, activation='softmax'))
+
+        # compile
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['top_k_categorical_accuracy'])
+
+        if verbose:
+            print(model.summary())
+
+        return model
+
+
+class ModelCreator:
+    path = None
+    model_name = None
+    model_prefix = "model_"
+    exts = ['.png', '.jpg', '.jpeg']
+    save_extension = ".png"
+
+    loader = DataLoader()
+    trainer = ModelTrain()
+
+    def __init__(self, path, model_name=None):
+        self.path = os.path.abspath(path)
+        self.__set_model_folder_name_or_default(model_name)
+
+    def train(self, name=None):
+
+        path = self.get_model_path()
+        names, (x_train, y_train), (x_test, y_test) = self.loader.load_training_data(path)
+
+        self.trainer.train(x_train, y_train)
+        self.trainer.evaluate(x_test, y_test)
+        self.trainer.save(name=name, path=path)
+
+    def save_component(self, name, images, replace=True, verbose=False):
+
+        """
+        Save component images in a folder named after the component.
+
+        :param name: component/folder name
+        :param images: component images
+        :param replace: if true, it will replace images from an existing folder
+        :param verbose: prints debug details
+        :return:
+        """
+
+        if not name:
+            print("Provided component name is not valid")
+            return
+        if images is None:
+            print("No images provided")
+            return
+
+        removed = 0
+        added = 0
+
+        path = create_folder_if_needed(self.get_model_path(), name)
+        current_images, current_images_size = list_images(path)
+
+        # remove previous
+        if replace and current_images_size > 0:
+            for image in current_images:
+                os.remove(image)
+                removed += 1
+            current_images, current_images_size = list_images(path)
+
+        for idx, image in enumerate(images):
+            image_name = "{}_{}{}".format(name, idx + current_images_size, self.save_extension)
+            image_path = os.path.join(path, image_name)
+
+            cv2.imwrite(image_path, image)
+            added += 1
+
+        current_images, current_images_size = list_images(path)
+
+        if verbose:
+            print("deleted: {}, added: {}, total: {} ({})".format(removed, added, current_images_size, path))
+
+    def remove_component(self, name):
+        """
+        Deletes component folder and it's content
+        :param name: component name
+        """
+
+        path = self.get_model_path()
+        component_path = os.path.join(path, name)
+        if os.path.exists(component_path):
+            shutil.rmtree(component_path)
+            return True
+        return False
+
+    def get_model_path(self):
+        return os.path.join(self.path, self.model_name)
+
+    def list_model_components(self):
+        return list_folders_with_images(self.get_model_path())
+
+    def __set_model_folder_name_or_default(self, name=None):
+        """
+        Creates component folder. If no name is provided and one is generated
+        :param name: component folder name
+        """
+        if not name:
+            folders, prefixed = self.__list_model_folders()
+            self.model_name = "{}{}".format(self.model_prefix, len(prefixed))
+        else:
+            self.model_name = name
+
+        create_folder_if_needed(self.path, self.model_name)
+
+    def __list_model_folders(self):
+        """
+        :return: a list of folders located in project root
+        """
+        folders = []
+        prefixed_folders = []
+        root = self.path
+        for item in os.listdir(root):
+            if os.path.isdir(os.path.join(root, item)) and not item.startswith("."):
+                if item.startswith(self.model_prefix):
+                    prefixed_folders.append(item)
+                folders.append(os.path.join(root, item))
+
+        return folders, prefixed_folders
+
+
