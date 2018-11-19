5c79e0a09b7188f3c57109f565718f04935dbbd4
Arthur Saveliev <arthur.saveliev@gmail.com>
2018-11-19 03:15:38 +0100

    sets detection screen as launch default

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 #!/usr/local/bin/python3
 
-from PyQt5 import QtWidgets, QtCore
-from ui.screen_edit_model import ScreenEditModel
+from PyQt5 import QtWidgets
+
+from ui.screen_detection import ScreenDetection
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    window = ScreenEditModel(None)
-    window.setWindowTitle('Add/Edit model')
+    # window = ScreenEditModel(None)
+    window = ScreenDetection(None)
+    window.setWindowTitle('Mobile screen detection')
     window.show()
     sys.exit(app.exec_())
