8914130c8dd495c13a1dd10248231fe18a74c719
Arthur Saveliev <arthur.saveliev@gmail.com>
2018-11-22 20:08:04 +0100

    tmp app2 to switch between screens

--- app2.py
+++ app2.py
@@ -3,12 +3,13 @@
 from PyQt5 import QtWidgets
 
 from ui.screen_detection import ScreenDetection
+from ui.screen_edit_model import ScreenEditModel
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    window = ScreenDetection(None)
-    window.setWindowTitle('Mobile screen detection')
+    window = ScreenEditModel(None)
+    window.setWindowTitle('Add/Edit model')
     window.show()
     sys.exit(app.exec_())
