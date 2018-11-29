82b8e7c4f424fb0356a8c01c5795a37b31532c00
Arthur Saveliev <arthur.saveliev@gmail.com>
2018-11-29 19:10:13 +0100

    export update

--- Test.js
+++ Test.js
@@ -1,9 +1,7 @@
-
-
 import React, { Component } from 'react'
 import { Text, View, ToolbarAndroid, StyleSheet, Image, FlatList } from 'react-native'
 
-export default class Test.js extends Component {
+export default class Test extends Component {
 
     static navigationOptions = { header: null }
 
@@ -12,33 +10,13 @@ export default class Test.js extends Component {
             <View style={{ marginTop: 24 }}>
 
 
-            <ToolbarAndroid 
-            style={styles.toolbar} 
-            title="Test title" 
-            navIcon={require("../assets/images/robot-dev.png")} />
-
-
-
             <Image style={{ width: 50, height: 50 }} source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
             />
 
 
 
-            <FlatList data={[{ key: 'This is item 1' }, { key: 'This is item 2' }]} renderItem={({ item }) =><Text>{item.key}</Text>}
-            />
 
-
-
-            <FlatList data={[{ key: 'This is item 1' }, { key: 'This is item 2' }]} renderItem={({ item }) =><Text>{item.key}</Text>}
-            />
-
-
-
-            <Button
-            onPress={onPressLearnMore}
-            title="Learn More"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
+            <Image style={{ width: 50, height: 50 }} source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
             />
 
 
@@ -53,5 +31,3 @@ const styles = StyleSheet.create({
         height: 56
     },
 });
-
-
