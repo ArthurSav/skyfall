a57aa46140c3358ad8def779b6b9884c62a5b294
Arthur Saveliev <arthur.saveliev@gmail.com>
2018-12-06 21:11:27 +0100

    image size export/train/eval global var

--- skyfall/engine/__init__.py
+++ skyfall/engine/__init__.py
@@ -1,6 +1,3 @@
-# from .contours import *
-# from .data_loading import *
-# from .train import *
-# from .predict import *
-
-# __all__ = ['Loader', 'Train', 'ContourFinder', 'Predict', 'CropType']
+IMAGE_SIZE_EXPORT = 64  # image size when saving training images
+IMAGE_SIZE_TRAIN = 64  # image size to resize to when importing & training a model
+IMAGE_SIZE_EVAL = 64  # image size to resize to when importing and evaluatiing a model
